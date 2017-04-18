204ad053714c9fda2199b8ee79296423132f52e5
Georgi Kolishovski <georgi.kolishovski@jax.org>
2017-04-18 12:39:22 -0400

    reordering functions and properties in the socplot.js file

--- src/soc/static/js/socplot.js
+++ src/soc/static/js/socplot.js
@@ -1,36 +1,36 @@
 "use strict";
 
+// COMMON PROPERTIES
+
+// colors for some common treatment groups are assigned in the database (table 'colors')
+// colors in the below list are assigned randomly to those less-common groups that 
+// do not have assigned special color values through the database
+
+// color codes from the below list should not repeat those in the database
+var colors = [
+    "#000000", 
+    "#1F78B4", 
+    "#B2DF8A", 
+    "#33A02C", 
+    "#FB9A99", 
+    "#E31A1C", 
+    "#FDBF6F", 
+    "#FF7F00", 
+    "#CAB2D6", 
+    "#6A3D9A"
+];
 
 
-/**
- * This function takes two parameters (x, y) and will return a number less than,
- * greater than or equal to zero depending on whether x is less than,
- * greater than or equal to y respectively. The '<' and '>' operators
- * are used to order x and y.
- * @param x
- * @param y
- * @return {number}
- */
-function compareBasic(x, y) {
-    if(x < y) {
-        return -1;
-    } else if(x > y) {
-        return 1;
-    } else {
-        return 0;
-    }
-}
+// ModeBar general settings 
+// can be extended in each individual plot module
+var modebar = {
+    displayModeBar: true, 
+    displaylogo: false, 
+    modeBarButtonsToRemove: ['sendDataToCloud']
+};
 
-/**
- * This function simply returns (x - y) and thus is a suitable comparison function
- * to use for sorting or searching numeric values
- * @param x {number}
- * @param y {number}
- * @return {number}
- */
-function compareNumeric(x, y) {
-    return x - y;
-}
+
+// COMMON METHODS
 
 /**
  * Binary search which returns the matchin index -(insertIndex + 1) in the case that no match is found
@@ -67,6 +67,39 @@ function binarySearch(array, x, compFunc) {
     return -(low + 1);
 }
 
+
+/**
+ * This function takes two parameters (x, y) and will return a number less than,
+ * greater than or equal to zero depending on whether x is less than,
+ * greater than or equal to y respectively. The '<' and '>' operators
+ * are used to order x and y.
+ * @param x
+ * @param y
+ * @return {number}
+ */
+function compareBasic(x, y) {
+    if(x < y) {
+        return -1;
+    } else if(x > y) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+
+/**
+ * This function simply returns (x - y) and thus is a suitable comparison function
+ * to use for sorting or searching numeric values
+ * @param x {number}
+ * @param y {number}
+ * @return {number}
+ */
+function compareNumeric(x, y) {
+    return x - y;
+}
+
+
 /**
  * Insert x into the given sorted array if it isn't already present (this function has no effect if the element is
  * already in the array).
@@ -86,6 +119,7 @@ function insertUnique(array, x, compFunc) {
         array.splice(idx, 0, x);
     }
 }
+
 
 /**
  *
@@ -113,6 +147,7 @@ function meanStddev(nums) {
     };
 }
 
+
 /**
  *
  * @param nums
@@ -125,16 +160,6 @@ function meanStderrStddev(nums) {
     return retVal;
 }
 
-var colors = [
-    "#000000", "#1F78B4", "#b2df8a", "#33a02c", "#fb9a99", "#e31a1c", "#fdbf6f",
-    "#FF7F00", "#cab2d6", "#6a3d9a"
-];
-
-var modebar = {
-    displayModeBar: true, 
-    displaylogo: false, 
-    modeBarButtonsToRemove: ['sendDataToCloud']
-};
 
 // we want to capture the measurements nearest the start/stop of treatment
 function findNearestMeasureDayIdx(uniqMeasurementDays, treatmentDay) {
@@ -158,10 +183,10 @@ function findNearestMeasureDayIdx(uniqMeasurementDays, treatmentDay) {
     return idx;
 }
 
+
 /**
 *  rounding decimal points in JavaScript is not stable;
-*  function should resolve this and be used in the application
-*  TO-DO: move to utilitites module
+*  this function should resolve this and be used in the application
 *  @param {numeric}
 *  @param {numeric}
 *  @return {numeric} 
